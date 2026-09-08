a4f362eff730397c30a0377bd5f62cba11b8ebf2
Tina Ayanda Fezani <fezaniayanda@gmail.com>
2026-09-09 01:02:49 +0200

    feat: add navigation structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import './App.css'
 
 function App() {
   return (
-    <main>
-      <h1>JobTrack</h1>
-      <p>Track your job applications in one place.</p>
-    </main>
+    <>
+      <nav className="navbar">
+        <h2 className="logo">JobTrack</h2>
+
+        <div className="nav-links">
+          <a href="#">Dashboard</a>
+          <a href="#">Applications</a>
+          <a href="#">Add Application</a>
+        </div>
+      </nav>
+
+      <main>
+        <h1>JobTrack</h1>
+        <p>Track your job applications in one place.</p>
+      </main>
+    </>
   )
 }
 
